1d1fcc19278f2aa2fa82d6840fbdaad151faa3ac
kiranprasadpathak <108648687+kiranprasadpathak@users.noreply.github.com>
2022-11-30 10:32:17 +0545

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+$num = 3;
+ 
+switch ($num) {
+    case 1:
+        echo "Your favorite number is 1";
+        break;
+    case 2:
+        echo "Your favorite number is 2";
+        break;
+    case 3:
+        echo "Your favorite number is 3";
+        break;
+    default:
+        echo "Your favorite number is not mentioned";
+}
